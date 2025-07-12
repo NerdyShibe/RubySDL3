25e5c51c4cb06c298fa85f82f0870fae447f0d7e
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-12 02:32:07 -0300

    feat: add main SDL3 module to load the libraries

---
+++ lib/sdl3.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'debug'
+require 'ffi'
+
+# Ruby Wrapper for SDL3
+#
+module SDL3
+  extend FFI::Library
+
+  # Load the library once here.
+  ffi_lib [
+    'libSDL3.so',       # Linux
+    'SDL3.dll',         # Windows
+    'libSDL3.dylib'     # macOS
+  ]
+end
+
+# Basics
+require_relative 'sdl3/basics/main'
+require_relative 'sdl3/basics/init'
