cfba18a98646b7816086886d40adf56ed90edf7e
Fernando Eufrásio <fernando.epm1@gmail.com>
2025-07-12 23:40:44 -0300

    feat: add all macros and enums for SDL_init.h

--- lib/sdl3/basics/init.rb
+++ lib/sdl3/basics/init.rb
@@ -1,28 +1,35 @@
 # frozen_string_literal: true
 
+# Wrapper for SDL_init.h
+# Handles Initialization and Shutdown
+#
 module SDL3
-  # Wrapper for SDL_init.h
-  # Handles Initialization and Shutdown
-  #
-  module Init
-    # Constants
-    SDL_INIT_AUDIO    = 0x00000010
-    SDL_INIT_VIDEO    = 0x00000020
-    SDL_INIT_JOYSTICK = 0x00000200
-    SDL_INIT_HAPTIC   = 0x00001000
-    SDL_INIT_GAMEPAD  = 0x00002000
-    SDL_INIT_EVENTS   = 0x00004000
-    SDL_INIT_SENSOR   = 0x00008000
-    SDL_INIT_CAMERA   = 0x00010000
+  # Macros
+  INIT_AUDIO    = 0x00000010
+  INIT_VIDEO    = 0x00000020
+  INIT_JOYSTICK = 0x00000200
+  INIT_HAPTIC   = 0x00001000
+  INIT_GAMEPAD  = 0x00002000
+  INIT_EVENTS   = 0x00004000
+  INIT_SENSOR   = 0x00008000
+  INIT_CAMERA   = 0x00010000
 
-    # Enums
-    SDL3.enum :sdl_app_result, [
-      :sdl_app_continue, 0,
-      :sdl_app_success,  1,
-      :sdl_app_failure,  2
-    ]
+  PROP_APP_METADATA_NAME_STRING       = 'SDL.app.metadata.name'
+  PROP_APP_METADATA_VERSION_STRING    = 'SDL.app.metadata.version'
+  PROP_APP_METADATA_IDENTIFIER_STRING = 'SDL.app.metadata.identifier'
+  PROP_APP_METADATA_CREATOR_STRING    = 'SDL.app.metadata.creator'
+  PROP_APP_METADATA_COPYRIGHT_STRING  = 'SDL.app.metadata.copyright'
+  PROP_APP_METADATA_URL_STRING        = 'SDL.app.metadata.url'
+  PROP_APP_METADATA_TYPE_STRING       = 'SDL.app.metadata.type'
 
-    SDL3.attach_function :init, :SDL_Init, [:uint32], :int
-    SDL3.attach_function :quit, :SDL_Quit, [], :void
-  end
+  # Enums
+  enum :app_result, %i[
+    app_continue
+    app_success
+    app_failure
+  ]
+
+  # Functions
+  attach_function :init, :SDL_Init, [:uint32], :int
+  attach_function :quit, :SDL_Quit, [], :void
 end
